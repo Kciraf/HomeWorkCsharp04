864809af5d82731cc0d1c295ec38b1c31d746e7e
Kciraf <valsinat44a@mail.ru>
2023-02-26 13:26:53 +0600

    Added exercise 27

--- Exercise27/Program.cs
+++ Exercise27/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumDigits(int number)
+{
+    int result = 0;
+    while (number >= 10)
+    {
+        result = result + number % 10;
+        number = number / 10;
+    }
+    result = result + number;
+    return result;
+}
+
+System.Console.Write("Введите число: ");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+System.Console.WriteLine("Сумма цифр в числе: " + SumDigits(num));
