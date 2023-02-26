3f520a9b4b271849196696198acee1ccf06b485b
Kciraf <valsinat44a@mail.ru>
2023-02-26 13:11:41 +0600

    Added exercise 25

--- Exercise25/Program.cs
+++ Exercise25/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+System.Console.Write("Введите число A для возведения его в степень: ");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число B - степень, в которую надо возвести число выше: ");
+int B = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+System.Console.WriteLine("Результат возведения числа A в степень В: " + Math.Pow(A, B));
